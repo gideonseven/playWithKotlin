e8db7972f2a943ad4c51895831fe4661d1f2831f
Gideon Tobing <gideon.tobing@gmail.com>
2026-04-22 21:24:52 +0800

    update two sum

--- src/main/kotlin/1.TwoSum.kt
+++ src/main/kotlin/1.TwoSum.kt
@@ -3,22 +3,21 @@ package com.gideon
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val result = mutableListOf<Int>()
-//    result.add(1)
 
-    val arr = intArrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-    var currentValue = 0
-    val target = 5
+    val nums = intArrayOf(3, 2, 4)
 
-
-    for (i in arr.indices) {
-        result.add(i)
-        currentValue += arr[i]
-        if (currentValue >= target) {
-            print("success")
-            println(currentValue)
-        } else {
-            println("not here yet")
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+        val results = mutableListOf<Int>()
+        for (i in nums.indices) {
+            for (j in i + 1 until nums.size) {
+                if (nums[i] + nums[j] == target) {
+                    return intArrayOf(i, j)
+                }
+            }
         }
+        return results.toIntArray()
     }
+
+
+    twoSum(nums, 6)
 }
