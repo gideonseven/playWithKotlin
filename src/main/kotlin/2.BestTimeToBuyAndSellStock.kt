f19f3129c46169a6c937e11e8e83477f857d1e3c
Gideon <gideon.tobing@gmail.com>
2026-04-27 17:16:40 +1000

    update answer

--- src/main/kotlin/2.BestTimeToBuyAndSellStock.kt
+++ src/main/kotlin/2.BestTimeToBuyAndSellStock.kt
@@ -4,41 +4,19 @@ fun main() {
 
     fun maxProfit(prices: IntArray): Int {
 
-//        val prices = intArrayOf(7,1,5,3,6,4)
-
-        var buyPos = 0
-        var sellPos = 0
-        var biggest = prices[prices.size - 1]
-        var smallest = prices[0]
-
-        // make it Integer
-        val median : Int = if(prices.size % 2 == 0) prices.size / 2 else prices.size / 2 + 1
-
-        println(" Our median is $median")
-
-        for (i in prices.indices) {
-            if (i <= median) {
-                if(prices[i] < smallest) {
-                    smallest = prices[i]
-                    buyPos = i
-                }
-            }
-            if(i >= median){
-                if(prices[i] > biggest) {
-                    biggest = prices[i]
-                    sellPos = i
-                }
+        var minPrice = Int.MAX_VALUE
+        var maxProfit = 0
+        for (price in prices){
+            if(price < minPrice){
+                minPrice = price
+            } else if (price - minPrice > maxProfit){
+                maxProfit = price - minPrice
             }
         }
 
-        val profit = prices[sellPos] - prices[buyPos]
-
-        println(profit)
-
-        return profit
+        return maxProfit
     }
 
-//    val prices = intArrayOf(7,1,5,3,6,4)
     val prices = intArrayOf(7,6,4,3,1)
 
     maxProfit(prices)
