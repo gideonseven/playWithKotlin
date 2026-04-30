f2ee7a652e958ff47bf8df8700faa11f32b59d78
Gideon Tobing <gideon.tobing@gmail.com>
2026-04-30 14:35:21 +0800

    add second solutin for valid anagram

--- src/main/kotlin/242.ValidAnagram.kt
+++ src/main/kotlin/242.ValidAnagram.kt
@@ -1,11 +1,16 @@
 package com.gideon
 
+import java.lang.Character.charCount
+
 fun main() {
 
     isAnagram("anagram", "nagaram")
+
 }
 
-fun isAnagram(s: String, t: String): Boolean {
+
+// solution 1
+/*fun isAnagram(s: String, t: String): Boolean {
     var isAnagram = false
 
     if (s.count() != t.count()) {
@@ -20,4 +25,18 @@ fun isAnagram(s: String, t: String): Boolean {
         isAnagram = true
     }
     return isAnagram
+}*/
+
+//solution 2
+fun isAnagram(s: String, t: String): Boolean {
+    if (s.length != t.length) return false
+
+    val charCount = HashMap<Char, Int>()
+
+    for(i in s.indices){
+        charCount.put(s[i], charCount.getOrDefault(s[i], 0) + 1)
+        charCount.put(t[i], charCount.getOrDefault(t[i], 0) - 1)
+    }
+
+    return charCount.values.all { it == 0 }
 }
